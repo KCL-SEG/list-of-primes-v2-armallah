f684aeb77ee0dcdbc66bf0c0b4bc6decdcd08e52
Abdul Rahman <abdul.mallah@kcl.ac.uk>
2022-11-14 22:44:47 +0000

    ...

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    n = 2
+    while len(list) != number_of_primes:
+        for i in range(2, n // 2 + 1):
+            if n % i == 0:
+                break
+        else:
+            list.append(n)
+        n += 1
     return list
+        
+
+
